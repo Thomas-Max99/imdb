c1c4ac87a5938ce52920f98c26d18aaf13775dfd
Thomas-Max99 <thomssentre@hotmail.com>
2023-03-10 10:03:42 +0000

    fetching the data from themoviedb

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,26 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+export const dynamic = "force-dynamic"; // this is the fix
 
-const inter = Inter({ subsets: ['latin'] })
+const API_KEY = process.env.API_KEY
+console.log("apikey", API_KEY);
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
 
-export default function Home() {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`);
+
+  console.log(res)
+  if(!res.ok){
+    throw new Error("Failed data fetch")
+  }
+
+  
+  const data = res.json();
+
+  const results = data.results;
+
+  // console.log(results);
   return (
    <h2 className="text-red-400">
      Hello
